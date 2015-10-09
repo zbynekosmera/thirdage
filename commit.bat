d491c249a059a86eec67ecf1ef077052c4420a55
Zbynek <zbynek.osmera@N0509.hcg.homecredit.net>
2015-10-09 13:53:23 +0200

    Dalsi uprava

--- commit.bat
+++ commit.bat
@@ -1,7 +1,9 @@
-echo start
+echo start commitu
 
 git commit -am "Dalsi uprava"
 
 git push
+
+echo commit hotov
 
 pause
--- update.bat
+++ update.bat
@@ -1,7 +1,9 @@
-echo start
+echo start update
 
 git fetch
 
 git merge origin/master
+
+echo update hotovo
 
 pause
